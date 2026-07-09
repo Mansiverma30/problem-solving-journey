6ded5cd225c015e26be8a8eb4134f61feaf41bbe
Mansi Verma <mansiv824@gmail.com>
2026-07-09 18:58:22 +0530

    decode done

--- Readme.md
+++ Readme.md
@@ -10,8 +10,8 @@ Building strong problem-solving skills through daily DSA practice. (am I????)
 
 ## Progress
 
-* Current Streak: 60 days (yayayaya 🎉)
-* Problems Solved: 48.5 (Done and Dusted. 💅)
+* Current Streak: 61 days (yayayaya 🎉)
+* Problems Solved: 49 (Done and Dusted. 💅)
 * Revision/Practice: 13 (for off days/while starting new ds)
 * Test: 2 (Weekly test hmmmmmmm)
 * Streak Freezze left (3 per month): 1
--- Stack/decode.js
+++ Stack/decode.js
@@ -4,15 +4,18 @@ function decode(s) {
     let currentNumber = 0
     for (const c of s) {
         if (c >= "0" && c <= "9") {
-            currentNumber = Number(c)
+            currentNumber = currentNumber * 10 + Number(c)
         }
         else if (c === "[") {
             stack.push(currentString)
             stack.push(currentNumber)
+            currentNumber = 0
+            currentString = ""
         }
         else if (c === "]") {
-            stack.pop()
-            stack.push(currentString.repeat(currentNumber))
+            let repeat = stack.pop()
+            let prevString = stack.pop()
+            currentString = prevString + currentString.repeat(repeat)
         }
         else {
             currentString += c
@@ -21,4 +24,7 @@ function decode(s) {
     console.log(currentString)
 }
 
-decode("3[a]")
+decode("3[a]")          // "aaa"
+decode("3[a2[c]]")      // "accaccacc"
+decode("2[abc]3[cd]ef") // "abcabccdcdcdef"
+decode("10[a]")         // "aaaaaaaaaa"
