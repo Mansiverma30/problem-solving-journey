202b6cb8518473b666525b38be11efe2c00f5e1c
Mansi Verma <mansiv824@gmail.com>
2026-07-22 19:20:46 +0530

    majority-elem

---
+++ Array/majority-elements.js
@@ -0,0 +1,16 @@
+function majority(nums) {
+    let ans = nums[0]
+    let count = 1
+    for (let i = 1; i < nums.length; i++) {
+        if (count === 0) {
+            ans = nums[i]
+            count++
+        }
+        else if (ans == nums[i]) {
+            count++
+        } else count--
+    }
+    return ans
+}
+
+console.log(majority([2, 2, 1, 1, 1, 2, 2]))
